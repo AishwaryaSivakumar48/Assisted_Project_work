d5ff3afc9c194f6825b8aecddb44cdd3094ba0ee
Aishwarya <aishwarya.sivakumar48@gmail.com>
2022-08-27 12:56:49 +0530

    phase-3

---
+++ HandlingUserAuthentication/src/main/java/com/project3/phase3/HandlingUserAuthentication/UserAuthentication.java
@@ -0,0 +1,18 @@
+package com.project3.phase3.HandlingUserAuthentication;
+
+//package com.simplilearn.demo;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "Aishwarya";
+	}
+	public String getEmail() {
+		return "aishwarya@gmail.com";
+	}
+	public int getPhnNum() {
+		return 987654321;
+	}
+	public String getPassword() {
+		return "@123";
+	}
+}
