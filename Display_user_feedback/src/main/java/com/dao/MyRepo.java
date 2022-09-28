a5bb198832984e10cbf576cf457b8f287eeda931
Aishwarya <aishwarya.sivakumar48@gmail.com>
2022-09-28 15:10:38 +0530

    new

--- Display_user_feedback/src/main/java/com/dao/MyRepo.java
+++ Display_user_feedback/src/main/java/com/dao/MyRepo.java
@@ -1,11 +1,12 @@
 package com.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.entity.Feedback;
 
 @Repository
-public interface MyRepo extends CrudRepository<Feedback, Integer>{
+public interface MyRepo extends JpaRepository<Feedback, Integer>{
 
 }
